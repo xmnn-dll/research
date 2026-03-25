913a68497a4993ec0b9bb8c30419d5cf8800ab0a
𝗩 <brokernoreply@proton.me>
2026-03-25 18:32:13 +0100

    __fastcall wil::details::in1diag5::_Log_Hr.c

---
+++ LocationApi/__fastcall wil::details::in1diag5::_Log_Hr.c
@@ -0,0 +1,20 @@
+void __fastcall wil::details::in1diag5::_Log_Hr(
+        wil::details::in1diag5 *this,
+        void *a2,
+        __int64 a3,
+        const char *a4,
+        const char *a5,
+        const char *a6)
+{
+  void *retaddr; // [rsp+48h] [rbp+0h]
+
+  wil::details::ReportFailure_Hr(
+    this,
+    a2,
+    "drivers\\mobilepc\\location\\product\\api\\winlegacyapi\\dllapi\\legacypositionrequest.cpp",
+    a4,
+    a5,
+    retaddr,
+    2,
+    (_DWORD)a6);
+}
